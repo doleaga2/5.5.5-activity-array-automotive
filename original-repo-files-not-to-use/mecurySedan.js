eb837d50f31b315bf76f13a31ca667fd7a5bc326
doleaga2 <oleaga.diana@gmail.com>
2022-11-07 18:34:34 -0500

    car

--- original-repo-files-not-to-use/mecurySedan.js
+++ original-repo-files-not-to-use/mecurySedan.js
@@ -1,6 +1,49 @@
+
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle");
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends VehicleModule.Vehicle{
+    
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+    }
+
+
+loadPassenger(num)
+{
+    if(this.maxPassengers > this.passenger){
+        this.num = (this.maxPassengers - this.passenger);
+    }
+}
+
+start()
+{
+    if(this.fuel > 0){
+        return this.start = true;
+    }
+    else{
+        return this.start = false;
+    }
+}
+
+scheduleService(mileage)
+{  
+    if(this.mileage > 30000){
+        return this.scheduleService = true;
+    }
+    else{
+        return this.scheduleService = false;
+    }
+}
+}
+let c = new Car ("Mecury", "Sedan", "1965", "color", "mileage");
+console.log (c)
